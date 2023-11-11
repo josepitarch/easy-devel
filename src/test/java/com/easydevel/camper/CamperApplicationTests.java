d927ae2bc785718245c3c13e0e16e9f4da74e728
jopimi <josepitarch98@gmail.com>
2023-11-11 17:17:53 +0100

    first commit

---
+++ src/test/java/com/easydevel/camper/CamperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easydevel.camper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
